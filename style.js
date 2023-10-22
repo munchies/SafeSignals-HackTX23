bac98adaa1aa903879fd9886729edce1c02f489a
munchies <124452561+munchies@users.noreply.github.com>
2023-10-22 10:41:48 -0500

    Create style.js

---
+++ style.js
@@ -0,0 +1,171 @@
+html {
+  height: 100%;
+  width: 100%;
+}
+
+/*Common among all pages*/
+/*Navigation Bar*/
+li {
+  display: inline;
+  float: right;
+}
+
+ul {
+  list-style-type: none;
+  background-color: #a3d5dc;
+  
+  overflow: hidden;
+}
+
+li a {
+  font-size: 30px;
+  font-family: cursive;
+  font-weight: 200;
+  color: #091781;
+  text-decoration: none;
+  padding: 30px;
+}
+
+li a:hover {
+  background-color: #80d6a9;
+  color: #ffffff;
+}
+
+body {
+ background-color: rgb(247, 216, 186); 
+}
+
+header{
+  font-family: cursive;
+  margin-top: 50px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 50px;
+  border-style: dotted;
+  border-width: 5px;
+  background-color:#a3d5dc ;
+}
+
+h1 {
+  font-family: cursive;
+  font-size: 70px;
+  font-weight: 600;
+  color: white;
+  text-align: center;
+  background-color: navy;
+}
+
+/*Home Page*/
+img {
+  display: block;
+  width: 40%;
+  height: auto;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 30px;
+  border-style: solid;
+  border-color: black;
+  border-width: 40px;
+}
+
+
+
+h2{
+  font-family: cursive;
+  font-size: 45px;
+  font-weight: 600;
+  color: #a28911;
+  text-align: left;
+  margin-top: 30px;
+  margin-bottom: 10px;
+}
+
+p{
+  
+  font-size: 30px;
+  font-family: 'Times New Roman', Times, serif;
+  font-style: italic;
+  font-weight: 600;
+  color: #a28911;
+  text-align: left;
+  margin-bottom: 10px;
+}
+
+label{
+  color: black;
+  font-weight: 600;
+}
+
+
+input{
+  width: 300px;
+  height: 30px;
+  border-radius: 5px;
+  border-color: #b90e0e;
+  border-style: solid;
+  margin-bottom: 10px;
+}
+.instruct {
+
+  font-size: 30px;
+  font-weight: 600;
+  color: navy;
+  text-align: left;
+  margin-bottom: 10px;
+}
+
+.theMission {
+
+  font-size: 30px;
+  font-weight: 600;
+  color: navy;
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.subtitle
+{
+  font-size: 35px;
+  font-style: italic;
+  font-family: 'Times New Roman', Times, serif;
+  font-weight: 600;
+  color: navy;
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.button{
+  background-color: #fafeff;
+  border: none;
+  color: black;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  width: 100px;
+}
+
+.commentsBox{
+  width: 600px;
+  height: 100px;
+  border-radius: 5px;
+  border-color: #b90e0e;
+  border-style: solid;
+  margin-bottom: 10px;
+  text-align: top;
+}
+
+h4{
+  font-family: cursive;
+  font-size: 20px;
+  font-weight: 600;
+  color: #a28911;
+  text-align: left;
+  margin-top: 50px;
+  margin-bottom: 10px;
+  
+}
