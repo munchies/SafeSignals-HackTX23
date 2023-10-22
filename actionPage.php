760c67a0c927f19b950060d1e57cf8c9ed04837a
munchies <124452561+munchies@users.noreply.github.com>
2023-10-22 10:42:20 -0500

    Create actionPage.php

---
+++ actionPage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>replit</title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+  </head>
+<body>
+  <nav>
+    <ul> 
+      <li><a href="Join the Team.html">Join the Team</a></li>
+      <li><a href="Mission.html">Our Mission</a></li>
+      <li><a href="Instructions.html">Instructions</a></li>
+      <li><a href="index.html">Home</a></li>
+
+    </ul>
+
+  </nav>
+  <p>Form submitted! We will reach out to you shortly.</p>
+
+</body>
+</html>
